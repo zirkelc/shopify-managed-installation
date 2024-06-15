def1fe83ab6c8e1bc8c2d12119c3dc01ddff3684
zirkelc <chris.zirkel@gmail.com>
2024-06-15 14:52:38 +0200

    first commit

---
+++ app/shopify.server.ts
@@ -0,0 +1,64 @@
+import "@shopify/shopify-app-remix/adapters/node";
+import {
+  ApiVersion,
+  AppDistribution,
+  DeliveryMethod,
+  shopifyApp,
+} from "@shopify/shopify-app-remix/server";
+import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
+import { restResources } from "@shopify/shopify-api/rest/admin/2024-04";
+import prisma from "./db.server";
+import { randomUUID } from "crypto";
+
+export const users: Array<{ id: string; name: string }> = [];
+
+const shopify = shopifyApp({
+  apiKey: process.env.SHOPIFY_API_KEY,
+  apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
+  apiVersion: ApiVersion.April24,
+  scopes: process.env.SCOPES?.split(","),
+  appUrl: process.env.SHOPIFY_APP_URL || "",
+  authPathPrefix: "/auth",
+  sessionStorage: new PrismaSessionStorage(prisma),
+  distribution: AppDistribution.AppStore,
+  restResources,
+  webhooks: {
+    APP_UNINSTALLED: {
+      deliveryMethod: DeliveryMethod.Http,
+      callbackUrl: "/webhooks",
+    },
+  },
+  hooks: {
+    afterAuth: async ({ session }) => {
+      console.log('-> afterAuth');
+
+      console.log('Creating user');
+      await new Promise<void>((resolve) => setTimeout(() => {
+        users.push({ id: randomUUID(), name: session.shop });
+
+        resolve();
+      }, 1_000));
+
+      console.log('User created');
+
+      shopify.registerWebhooks({ session });
+
+      console.log('<- afterAuth');
+    },
+  },
+  future: {
+    unstable_newEmbeddedAuthStrategy: true,
+  },
+  ...(process.env.SHOP_CUSTOM_DOMAIN
+    ? { customShopDomains: [process.env.SHOP_CUSTOM_DOMAIN] }
+    : {}),
+});
+
+export default shopify;
+export const apiVersion = ApiVersion.April24;
+export const addDocumentResponseHeaders = shopify.addDocumentResponseHeaders;
+export const authenticate = shopify.authenticate;
+export const unauthenticated = shopify.unauthenticated;
+export const login = shopify.login;
+export const registerWebhooks = shopify.registerWebhooks;
+export const sessionStorage = shopify.sessionStorage;
